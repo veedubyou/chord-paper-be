785bb97ef632e4c02bed0f30a543b67cf51449e3
veedubyou <vincentwhwoo@gmail.com>
2020-09-11 02:16:54 -0700

    moving auth payload into header (#17)

--- src/gateway/login.rs
+++ src/gateway/login.rs
@@ -1,15 +1,10 @@
-use serde::Deserialize;
-
+use super::auth;
 use crate::usecases;
+use crate::usecases::login::User;
 use http::StatusCode;
 
 pub struct Google {
     usecase: usecases::login::Google,
-}
-
-#[derive(Deserialize)]
-pub struct VerifyLoginRequest {
-    id_token: String,
 }
 
 impl Google {
@@ -19,12 +14,25 @@ impl Google {
         }
     }
 
-    pub fn verify_login(&self, request: VerifyLoginRequest) -> Box<dyn warp::Reply> {
-        let result = self.usecase.verify_login(request.id_token.as_str());
+    pub fn verify_login(&self, user_id: &str, auth_header_value: &str) -> Box<dyn warp::Reply> {
+        let token: String = match auth::extract_bearer_token(&auth_header_value) {
+            Ok(auth_token) => auth_token,
+            Err(error) => {
+                return Box::new( warp::reply::with_status(error.to_string(), StatusCode::UNAUTHORIZED))
+            }
+        };
 
-        match result {
-            Ok(user) => Box::new(warp::reply::json(&user)),
-            Err(error) => Box::new( warp::reply::with_status(error.to_string(), StatusCode::UNAUTHORIZED)),
+        let user: User = match self.usecase.verify_login(&token) {
+            Ok(user) => user,
+            Err(error) => {
+                return Box::new( warp::reply::with_status(error.to_string(), StatusCode::UNAUTHORIZED))
+            },
+        };
+
+        if user.id != user_id {
+            return Box::new( warp::reply::with_status("Requested user ID does not match id token", StatusCode::UNAUTHORIZED))
         }
+
+        Box::new(warp::reply::json(&user))
     }
 }
