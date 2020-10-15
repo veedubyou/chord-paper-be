43353219f58dc28b85560bb77ecd785ab2d369aa
Vincent <vincentwhwoo@gmail.com>
2020-10-14 21:02:00 -0700

    impl update song

--- src/application/songs/gateway.rs
+++ src/application/songs/gateway.rs
@@ -38,14 +38,33 @@ impl Gateway {
             Err(err) => map_usecase_errors(err),
         }
     }
+
+    pub async fn update_song(
+        &self,
+        auth_header_value: &str,
+        song_id: &str,
+        song: entity::Song,
+    ) -> Box<dyn warp::Reply> {
+        let token: String = match auth::extract_auth_token(&auth_header_value) {
+            Ok(token) => token,
+            Err(reply) => return reply,
+        };
+
+        let update_song_result = self.usecase.update_song(&token, song_id, song).await;
+
+        match update_song_result {
+            Ok(song) => Box::new(warp::reply::json(&song)),
+            Err(err) => map_usecase_errors(err),
+        }
+    }
 }
 
 fn map_usecase_errors(err: usecase::Error) -> Box<dyn warp::Reply> {
     let status_code = match err {
         usecase::Error::VerificationError { .. } => http::StatusCode::UNAUTHORIZED,
-        usecase::Error::ExistingSongError | usecase::Error::WrongOwnerError => {
-            http::StatusCode::BAD_REQUEST
-        }
+        usecase::Error::ExistingSongError
+        | usecase::Error::WrongOwnerError
+        | usecase::Error::WrongIDError { .. } => http::StatusCode::BAD_REQUEST,
         usecase::Error::NotFoundError { .. } => http::StatusCode::NOT_FOUND,
         usecase::Error::DatastoreError { .. } => http::StatusCode::INTERNAL_SERVER_ERROR,
     };
