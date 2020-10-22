7892fac124b75c5ece01d7a5f9649733b977266b
Vincent <vincentwhwoo@gmail.com>
2020-10-21 23:20:17 -0700

    filter out "Type" field

--- src/application/concerns/gateway/errors.rs
+++ src/application/concerns/gateway/errors.rs
@@ -7,7 +7,21 @@ pub trait GatewayError {
 }
 
 pub fn error_reply(error: Box<dyn GatewayError>) -> Box<dyn warp::Reply> {
-    let json = Box::new(warp::reply::json(&error));
+    let mut value: serde_json::Value = match serde_json::to_value(&error) {
+        Ok(value) => value,
+        Err(err) => panic!("Gateway Error should be serializable!: {}", err),
+    };
+
+    let serde_object = match value.as_object_mut() {
+        Some(object) => {
+            // removing the type field, typetag doesn't expose an option to leave it untagged
+            object.remove("type");
+            object
+        }
+        None => panic!("Unexpected, serialized GatewayError cannot be made into a Hashmap"),
+    };
+
+    let json = Box::new(warp::reply::json(serde_object));
     Box::new(warp::reply::with_status(json, error.status_code()))
 }
 
