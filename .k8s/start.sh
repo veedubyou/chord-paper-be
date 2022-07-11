9266e1f31579fd3cab76a32ce980a4e8b26964d1
pw1124 <56487540+pw1124@users.noreply.github.com>
2022-07-11 11:47:48 -0700

    golang proxy service (#75)
    Co-authored-by: Phil <phil@phil>

--- .k8s/start.sh
+++ .k8s/start.sh
@@ -2,6 +2,24 @@
 
 # Convenience script to deploy everything
 
-kubectl apply -f "deploy.yml"
+# gets latest image from dockerhub
+export LATEST_GO_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/pw1124/chord-be-go/tags/?page_size=1000" | \
+        jq '.results | .[] | .name' -r | \
+        sed 's/latest//' | \
+        sort --version-sort | \
+        tail -n 1)
+
+export LATEST_RUST_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/pw1124/chord-be-rust/tags/?page_size=1000" | \
+        jq '.results | .[] | .name' -r | \
+        sed 's/latest//' | \
+        sort --version-sort | \
+        tail -n 1)
+
+printenv LATEST_GO_IMAGE
+printenv LATEST_RUST_IMAGE
+
+cat ./deploy.yml | envsubst > ./processed-yamls/deploy.yml
+
+kubectl apply -f "./processed-yamls/deploy.yml"
 kubectl apply -f "service.yml"
 watch kubectl get all
