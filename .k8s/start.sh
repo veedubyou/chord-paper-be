8249a5e2fbba39e6122d5790dfd7e636ea0d181c
pw1124 <56487540+pw1124@users.noreply.github.com>
2022-08-10 14:49:22 -0700

    Housekeeping new runners (#107)
    * docker image prune no longer needed as runners are now ephemeral
    
    * old runner workflows no longer needed
    
    * add network=host to solve issues with k8s/dind/alpine - see https://stackoverflow.com/questions/59503454/gitlab-pipeline-docker-build-gets-stuck-on-apk
    
    * remove network=host to try MTU, remove rust deployment
    
    * removed now irrelevant rust parts
    
    Co-authored-by: Phil <phil@phil>

--- .k8s/start.sh
+++ .k8s/start.sh
@@ -9,14 +9,7 @@ export LATEST_GO_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/
         sort --version-sort | \
         tail -n 1)
 
-export LATEST_RUST_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/pw1124/chord-be-rust/tags/?page_size=1000" | \
-        jq '.results | .[] | .name' -r | \
-        sed 's/latest//' | \
-        sort --version-sort | \
-        tail -n 1)
-
 printenv LATEST_GO_IMAGE
-printenv LATEST_RUST_IMAGE
 
 cat ./deploy.yml | envsubst > ./processed-yamls/deploy.yml
 
--- .k8s/stop-rust.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Convenience script to undeploy everything
-
-kubectl delete svc/chord-be-rust
-kubectl delete deploy/chord-be-rust
-watch kubectl get all
