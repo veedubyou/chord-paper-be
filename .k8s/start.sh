8a29a02cce63eeab1e0c36f555c0f5579b3bf8ff
pw1124 <56487540+pw1124@users.noreply.github.com>
2022-08-10 15:27:26 -0700

    Housekeeping new runners (#108)
    * docker image prune no longer needed as runners are now ephemeral
    
    * old runner workflows no longer needed
    
    * add network=host to solve issues with k8s/dind/alpine - see https://stackoverflow.com/questions/59503454/gitlab-pipeline-docker-build-gets-stuck-on-apk
    
    * remove network=host to try MTU, remove rust deployment
    
    * removed now irrelevant rust parts
    
    * changed everything back to using chord-be instead of chord-be-rust and chord-be-go
    
    * remove network=host
    
    Co-authored-by: Phil <phil@phil>

--- .k8s/start.sh
+++ .k8s/start.sh
@@ -3,13 +3,13 @@
 # Convenience script to deploy everything
 
 # gets latest image from dockerhub
-export LATEST_GO_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/pw1124/chord-be-go/tags/?page_size=1000" | \
+export LATEST_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/pw1124/chord-be/tags/?page_size=1000" | \
         jq '.results | .[] | .name' -r | \
         sed 's/latest//' | \
         sort --version-sort | \
         tail -n 1)
 
-printenv LATEST_GO_IMAGE
+printenv LATEST_IMAGE
 
 cat ./deploy.yml | envsubst > ./processed-yamls/deploy.yml
 
