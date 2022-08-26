23fdfac7e4aa6379d5f7de57c3e3e2d3526f1e59
Phil <phil@phil>
2022-08-26 00:55:15 -0700

    refactored all env vars into ConfigMap and aggregated all secrets

--- .k8s/worker/start.sh
+++ .k8s/worker/start.sh
@@ -2,5 +2,24 @@
 
 # Convenience script to deploy everything
 
-kubectl apply -f "deploy.yml" --record
+# gets latest image from dockerhub
+export LATEST_WORKERS_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/pw1124/chord-be-workers/tags/?page_size=1000" | \
+        jq '.results | .[] | .name' -r | \
+        sed 's/latest//' | \
+        sort --version-sort | \
+        tail -n 1)
+export LATEST_YOUTUBE_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/pw1124/youtube-dl-bin/tags/?page_size=1000" | \
+        jq '.results | .[] | .name' -r | \
+        sed 's/latest//' | \
+        sort --version-sort | \
+        tail -n 1)
+
+printenv LATEST_WORKERS_IMAGE
+printenv LATEST_YOUTUBE_IMAGE
+
+mkdir -p ./processed-yamls/ && cat ./deploy.yml | envsubst > ./processed-yamls/deploy.yml
+
+kubectl apply -f "../common/chord-be-common-cm.yaml"
+kubectl apply -f "./processed-yamls/deploy.yml"
+
 watch kubectl get all
