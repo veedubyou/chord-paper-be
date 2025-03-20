2eab65b4ee012c499243a15861b5b344aa76559b
Vincent Woo <vincentwhwoo@gmail.com>
2025-03-20 01:11:57 -0700

    User verification (#160)
    * wip
    
    * Update errors.go
    
    * tests
    
    * Update user_test.go
    
    * wip
    
    * Update user_test.go
    
    * ok
    
    * Update user_test.go
    
    * one more test

--- src/server/internal/user/user_test.go
+++ src/server/internal/user/user_test.go
@@ -1,9 +1,11 @@
 package user_test
 
 import (
+	"context"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/veedubyou/chord-paper-be/src/server/internal/shared_tests/auth"
+	userentity "github.com/veedubyou/chord-paper-be/src/server/internal/user/entity"
 	"github.com/veedubyou/chord-paper-be/src/server/internal/user/gateway"
 	"github.com/veedubyou/chord-paper-be/src/server/internal/user/storage"
 	"github.com/veedubyou/chord-paper-be/src/server/internal/user/usecase"
@@ -14,13 +16,14 @@ import (
 
 var _ = Describe("User", func() {
 	var (
+		userStorage userstorage.DB
 		userGateway usergateway.Gateway
 		validator   testing.Validator
 	)
 
 	BeforeEach(func() {
 		validator = testing.Validator{}
-		userStorage := userstorage.NewDB(db)
+		userStorage = userstorage.NewDB(db)
 		userUsecase := userusecase.NewUsecase(userStorage, validator)
 		userGateway = usergateway.NewGateway(userUsecase)
 	})
@@ -36,6 +39,60 @@ var _ = Describe("User", func() {
 			})
 
 			authtest.ItRejectsUnauthorizedRequests("POST", "/login")
+		})
+
+		Describe("For an unverified user not yet in DB", func() {
+			var (
+				response *httptest.ResponseRecorder
+			)
+
+			BeforeEach(func() {
+				request := testing.RequestFactory{
+					Method:  "POST",
+					Target:  "/login",
+					JSONObj: nil,
+					Mods:    testing.RequestModifiers{testing.WithUserCred(testing.UnverifiedUserNotInDB)},
+				}.MakeFake()
+				response = httptest.NewRecorder()
+
+				c := testing.PrepareEchoContext(request, response)
+				err := userGateway.Login(c)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("returns 401", func() {
+				Expect(response.Code).To(Equal(http.StatusUnauthorized))
+			})
+
+			It("commits the user to DB", func() {
+				Eventually(func() (userentity.User, error) {
+					return userStorage.GetUser(context.Background(), testing.UnverifiedUserNotInDB.ID)
+				}).Should(BeEquivalentTo(testing.UnverifiedUserNotInDB))
+			})
+		})
+
+		Describe("For an unverified user that is in DB", func() {
+			var (
+				response *httptest.ResponseRecorder
+			)
+
+			BeforeEach(func() {
+				request := testing.RequestFactory{
+					Method:  "POST",
+					Target:  "/login",
+					JSONObj: nil,
+					Mods:    testing.RequestModifiers{testing.WithUserCred(testing.UnverifiedUserInDB)},
+				}.MakeFake()
+				response = httptest.NewRecorder()
+
+				c := testing.PrepareEchoContext(request, response)
+				err := userGateway.Login(c)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("returns 401", func() {
+				Expect(response.Code).To(Equal(http.StatusUnauthorized))
+			})
 		})
 
 		Describe("For an authorized user", func() {
@@ -63,7 +120,10 @@ var _ = Describe("User", func() {
 
 			It("returns the correct user", func() {
 				userResponse := testing.DecodeJSON[usergateway.UserJSON](response.Body)
-				Expect(userResponse).To(BeEquivalentTo(testing.PrimaryUser))
+
+				Expect(userResponse.ID).To(Equal(testing.PrimaryUser.ID))
+				Expect(userResponse.Name).To(Equal(testing.PrimaryUser.Name))
+				Expect(userResponse.Email).To(Equal(testing.PrimaryUser.Email))
 			})
 		})
 	})
--- src/shared/testing/users.go
+++ src/shared/testing/users.go
@@ -12,30 +12,52 @@ import (
 var (
 	// in the system, is Google validated, and owner of songs and tracklists
 	PrimaryUser = User{
-		ID:    "primary-user-id",
-		Name:  "Primary User Name",
-		Email: "primary@chordpaper.com",
+		ID:       "primary-user-id",
+		Name:     "Primary User Name",
+		Email:    "primary@chordpaper.com",
+		Verified: true,
 	}
 
 	// in the system, is Google validated, but not owner of songs and tracklists
 	OtherUser = User{
-		ID:    "other-user-id",
-		Name:  "Other User Name",
-		Email: "other@chordpaper.com",
+		ID:       "other-user-id",
+		Name:     "Other User Name",
+		Email:    "other@chordpaper.com",
+		Verified: true,
+	}
+
+	// is Google validated, but not verified and should not have access
+	// also not saved to the DB
+	UnverifiedUserNotInDB = User{
+		ID:       "unverified-user-id",
+		Name:     "Unverified User Name",
+		Email:    "unverified@chordpaper.com",
+		Verified: false,
+	}
+
+	// is Google validated, but not verified and should not have access
+	// but saved to the DB
+	UnverifiedUserInDB = User{
+		ID:       "unverified-user-id-in-db",
+		Name:     "Unverified User Name in DB",
+		Email:    "unverified-in-db@chordpaper.com",
+		Verified: false,
 	}
 
 	// is Google validated but not in the system
 	NoAccountUser = User{
-		ID:    "not-in-db-id",
-		Name:  "Not In DB User",
-		Email: "adude@someoneelse.com",
+		ID:       "not-in-db-id",
+		Name:     "Not In DB User",
+		Email:    "adude@someoneelse.com",
+		Verified: false,
 	}
 
 	// not Google validated, also not in the system
 	GoogleUnauthorizedUser = User{
-		ID:    "google-unauthorized-user-id",
-		Name:  "Google Unauthorized User",
-		Email: "rando@notpaper.com",
+		ID:       "google-unauthorized-user-id",
+		Name:     "Google Unauthorized User",
+		Email:    "rando@notpaper.com",
+		Verified: false,
 	}
 )
 
@@ -48,7 +70,7 @@ var _ google_id.Validator = Validator{}
 type Validator struct{}
 
 func (t Validator) ValidateToken(ctx context.Context, requestToken string) (google_id.User, error) {
-	validatedUsers := []User{PrimaryUser, OtherUser, NoAccountUser}
+	validatedUsers := []User{PrimaryUser, OtherUser, UnverifiedUserNotInDB, NoAccountUser}
 
 	for _, validatedUser := range validatedUsers {
 		if requestToken == TokenForUserID(validatedUser.ID) {
@@ -66,6 +88,7 @@ func (t Validator) ValidateToken(ctx context.Context, requestToken string) (goog
 func EnsureUsers(db dynamolib.DynamoDBWrapper) {
 	EnsureUser(db, PrimaryUser)
 	EnsureUser(db, OtherUser)
+	EnsureUser(db, UnverifiedUserInDB)
 }
 
 func EnsureUser(db dynamolib.DynamoDBWrapper, u User) {
